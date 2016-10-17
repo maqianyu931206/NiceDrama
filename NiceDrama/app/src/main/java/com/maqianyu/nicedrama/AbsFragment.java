a8aaa35eeff3e89166641baeb2d62c8ddb50e217
qiansha <pangmeilnyx@163.com>
2016-10-17 17:28:05 +0800

    添加了Fragment的基类

---
+++ NiceDrama/app/src/main/java/com/maqianyu/nicedrama/AbsFragment.java
@@ -0,0 +1,61 @@
+package com.maqianyu.nicedrama;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+/**
+ * Created by dllo on 16/10/17.
+ * Fragment的基类
+ */
+public abstract class AbsFragment extends Fragment{
+    protected Context context;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        return inflater.inflate(setLayout(), container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initDatas();
+    }
+
+    protected abstract int setLayout();
+
+    protected abstract void initViews();
+
+    protected abstract void initDatas();
+
+
+    //简化findViewById
+    protected <T extends View> T byView(int resId) {
+        return (T) getView().findViewById(resId);
+    }
+//
+//    protected void goTo(Class<? extends AbsBaseActivity> to) {
+//        context.startActivity(new Intent(context, to));
+//    }
+
+}
