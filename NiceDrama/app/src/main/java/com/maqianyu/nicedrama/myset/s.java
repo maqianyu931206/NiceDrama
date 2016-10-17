7eee82e08515bf4abedba4302ddaf20750eaca6a
dllo <1658666520@qq.com>
2016-10-17 16:57:29 +0800

    创建工程

---
+++ NiceDrama/app/src/main/java/com/maqianyu/nicedrama/myset/s.java
@@ -0,0 +1,7 @@
+package com.maqianyu.nicedrama.myset;
+
+/**
+ * Created by dllo on 16/10/17.
+ */
+public class s {
+}
---
+++ NiceDrama/app/src/main/java/com/maqianyu/nicedrama/video/v.java
@@ -0,0 +1,7 @@
+package com.maqianyu.nicedrama.video;
+
+/**
+ * Created by dllo on 16/10/17.
+ */
+public class v {
+}
