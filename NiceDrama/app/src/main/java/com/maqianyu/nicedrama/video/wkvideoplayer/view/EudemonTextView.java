8d8bef3ccc46b9f57e5187411b6ddb0e2f30789d
ZhangHongdiL <1205304542@qq.com>
2016-10-28 14:16:08 +0800

    修改自定义View

--- NiceDrama/app/src/main/java/com/maqianyu/nicedrama/video/wkvideoplayer/view/EudemonTextView.java
+++ NiceDrama/app/src/main/java/com/maqianyu/nicedrama/video/wkvideoplayer/view/EudemonTextView.java
@@ -10,13 +10,13 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.maqianyu.nicedrama.R;
+
 /**
  * Created by dllo on 16/10/26.
  * 自定义View标签
  */
 public class EudemonTextView extends TextView {
-
-    private Paint paint;
 
     public EudemonTextView(Context context) {
         super(context);
@@ -29,21 +29,25 @@ public class EudemonTextView extends TextView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        paint = new Paint();
+
+        int width = getWidth();
+        int height = getHeight();
+
+        Paint paint = new Paint();
         paint.setColor(Color.rgb(231,65,27));
-        Rect rect3 = new Rect(-100, 35, 120, 65);
-//        //设置画笔的填充方式
+        Rect rect = new Rect(-100, height/4, width, height/2);
+        //设置画笔的填充方式
         paint.setStyle(Paint.Style.FILL);
         paint.setStrokeWidth(10);
         canvas.rotate(-45);
-        canvas.drawRect(rect3, paint);
-//        canvas.restore();
+        canvas.drawRect(rect, paint);
+        canvas.restore();
         Path path = new Path();
-        path.moveTo(10, 55);
-        path.lineTo(55, 10);
+        path.moveTo(10f, (float)(height/2));
+        path.lineTo((float)(height/2), 10f);
         Paint paint1 = new Paint();
         paint1.setColor(Color.WHITE);
-        paint1.setTextSize(15);
-        canvas.drawTextOnPath("守护神",path,10,10,paint1);
+        paint1.setTextSize(20);
+        canvas.drawTextOnPath("守护神", path, 5, 5, paint1);
     }
 }
